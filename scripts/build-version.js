6d3c5688ab86a0d5d69a8f5265d9aad59bc7c2da
d0252422-oss <d0252422@gmail.com>
2026-09-26 05:59:14 +0800

    feat(web): expose manual record editing

--- scripts/build-version.js
+++ scripts/build-version.js
@@ -1,7 +1,7 @@
 (function (root) {
   'use strict';
 
-  const BUILD_ID = '20260926-wearable-readback-03';
+  const BUILD_ID = '20260926-manual-edit-sync-04';
   const RECOVERY_PREFIX = 'healthCompanionBuildRecovery:';
 
   function recoveryUrl(href, liveBuildId) {
